e053585d706b202f90d63bdad5963c93655fa02e
paterson1720 <lesage1720@gmail.com>
2023-02-11 15:06:08 -0300

    add user loading state

--- src/components/Protected.js
+++ src/components/Protected.js
@@ -1,8 +1,17 @@
+import { CircularProgress } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useUser } from '../contexts/user-context';
 
 export default function Protected({ children }) {
   const { user } = useUser();
+  if (!user.loading) {
+    return (
+      <div style={{ display: 'grid', placeItems: 'center' }}>
+        <CircularProgress />
+      </div>
+    );
+  }
+
   if (!user.authToken) {
     return (
       <div style={{ display: 'grid', placeItems: 'center' }}>
--- src/contexts/user-context.js
+++ src/contexts/user-context.js
@@ -3,12 +3,13 @@ import * as React from 'react';
 const UserContext = React.createContext();
 
 function UserProvider({ children }) {
-  const [user, setUser] = React.useState({ authToken: null });
+  const [user, setUser] = React.useState({ authToken: null, loading: true });
   const value = { user, setUser };
 
   React.useEffect(() => {
     const token = localStorage.getItem('auth-token');
-    if (token) setUser({ authToken: token });
+    if (token) setUser({ authToken: token, loading: false });
+    else setUser({ authToken: null, loading: false });
   }, []);
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
